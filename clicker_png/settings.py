73d9ad2ac5fd7f913cbb3ec6a72ad9b426d9a3bd
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-03-07 18:09:16 +0100

    added .env for SECRET_KEY

--- clicker_png/settings.py
+++ clicker_png/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sir3)l+d0xtuoq@7o&7xo&(fcf^hgl9ry^lza=ku_1p0nk%oz$'
+load_dotenv()
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
