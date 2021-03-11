c9ee3596cc74e7b6c93ac1d8e618f7604addf28f
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-03-11 15:17:53 +0100

    Added 3 models to models.py

--- clicker_app/models.py
+++ clicker_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import BooleanField, CharField, IntegerField
 
-# Create your models here.
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+class Upgrade(models.Model):
+    name = CharField(max_length=128)
+    cost = IntegerField(default=6969) # Noticeable default value to be overriden
+    effect = IntegerField(default=1)
+    auto_click = BooleanField(default=True)
+
+    def  __str__(self):
+        return self.name
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    password = models.CharField()
+    achievements = models.ManyToManyField(Achievement)
+    upgrades = models.ManyToManyField(Upgrade)
+
+    def __str__(self):
+        return self.user.username
