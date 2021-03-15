47475bfd18fb3bc87b70875b482dc052d8f60c07
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-03-15 18:33:56 +0100

    Added TEMPLATE_DIR path variable

--- clicker_png/settings.py
+++ clicker_png/settings.py
@@ -17,6 +17,9 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# The directory containing all the templates used in each app
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -57,7 +60,7 @@ ROOT_URLCONF = 'clicker_png.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
