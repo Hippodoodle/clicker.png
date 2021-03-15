97800831aa3f3652893967d20c1ff58de2daccd4
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-03-15 18:34:29 +0100

    Added blank url behaviour

--- clicker_png/urls.py
+++ clicker_png/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from clicker_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('clicker_app/', include('clicker_app.urls')),
+    path('admin/', admin.site.urls),
 ]
