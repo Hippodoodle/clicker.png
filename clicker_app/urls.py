5c9e54a37ddfd4aa90f2751fce71a6595edf59ca
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-03-15 18:33:04 +0100

    Cleanup

--- clicker_app/urls.py
+++ clicker_app/urls.py
@@ -4,10 +4,10 @@ from clicker_app import views
 app_name = 'clicker_app'
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('about/',views.about,name='about'),
-    path('tutorial/',views.tutorial,name='tutorial'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('tutorial/', views.tutorial, name='tutorial'),
     #path('login/',views.login,name='login'),
-    path('signup/',views.signup,name='signup'),
-    path('myaccount/',views.myaccount,name='myaccount'),
+    path('signup/', views.signup, name='signup'),
+    path('myaccount/', views.myaccount, name='myaccount'),
 ]
--- clicker_app/views.py
+++ clicker_app/views.py
@@ -1,20 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'clicker_app/base.html')
+    response = render(request, 'clicker_app/base.html')
+    return response
+
 
 def about(request):
     return HttpResponse("This is the about page")
 
+
 def tutorial(request):
     return HttpResponse("This is the tutorial page")
+
 
 def login(request):
     return HttpResponse("This is the login page")
 
+
 def signup(request):
     return HttpResponse("This is the signup page")
+
 
 def myaccount(request):
     return HttpResponse("This is the myaccount page")
