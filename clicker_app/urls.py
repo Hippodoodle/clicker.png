90d88d7876eb642401f48c5fa6e8a93e4e0314af
Julia Szuminska <2472129s@student.gla.ac.uk>
2021-03-17 14:27:45 +0000

    Basic html structure

--- clicker_app/urls.py
+++ clicker_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('tutorial/', views.tutorial, name='tutorial'),
-    #path('login/',views.login,name='login'),
+    path('login/',views.login,name='login'),
     path('signup/', views.signup, name='signup'),
     path('myaccount/', views.myaccount, name='myaccount'),
 ]
--- clicker_png/settings.py
+++ clicker_png/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # The directory containing all the templates used in each app
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+# The directory containing static media files
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -124,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
