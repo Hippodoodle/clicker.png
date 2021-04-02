0f9cf6f7123fe767d3b2e8b121971ba670e330cb
Thomas McCausland <2472525m@student.gla.ac.uk>
2021-04-02 18:09:32 +0100

    Added broken clicker function and fancy button hover stuff

--- static/js/clicker-ajax.js
+++ static/js/clicker-ajax.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
-    $('#about-btn').click(function () {
+
+    $('#clicker').click(function () {
+        var username;
+        username = $(this).attr('data-user');
+
+        $.get('/clicker_app/add_points/', {'username': username},
+            function (data) {
+                $('#points_count').html(data);
+            })
+    });
+
+    $('button').hover(
+        function () {
+            $(this).css('color', 'red');
+        },
+        function () {
+            $(this).css('color', 'black');
+        });
+
+
+    $('#about-btn').click(function() {
         alert('You clicked the button using JQuery!');
     });
+
 });
