efa49b7966e607393f9454080c1d8fdd164a26a5
Thomas McCausland <thomas.mccausland11@gmail.com>
2023-07-18 20:56:28 +0100

    Fix jquery so that all fields update on auto click
    Also linted javascript for consistency and changed updated elements to
    class tags instead of id tags.

--- static/js/clicker-ajax.js
+++ static/js/clicker-ajax.js
@@ -1,58 +1,58 @@
 $(document).ready(function () {
 
-    $('#clicker').click(function () {
+    $("#clicker").click(function () {
         let a, clicks;
-        a = $(this).attr('data-user');
-        clicks = $(this).attr('data-click-upgraded');
-        const token = $(this).attr('data-csrf');
+        a = $(this).attr("data-user");
+        clicks = $(this).attr("data-click-upgraded");
+        const token = $(this).attr("data-csrf");
 
         $.ajax({
-            url: '/clicker_app/add_points/',
-            type: 'POST',
+            url: "/clicker_app/add_points/",
+            type: "POST",
             success: function (data) {
-                $('#points_count').html(data.points);
-                $('#user-counter').html(data.lifetime_points);
+                $(".points-count").html(data.points);
+                $(".lifetime-points").html(data.lifetime_points);
             },
-            headers: { 'X-CSRFToken': token },
-            data: { 'a': a, 'clicks': clicks }
+            headers: { "X-CSRFToken": token },
+            data: { "a": a, "clicks": clicks }
         })
     });
 
-    $('#darkmode-button').click(function () {
+    $("#darkmode-button").click(function () {
         let d;
-        d = $(this).attr('data-user');
-        const token = $(this).attr('data-csrf');
+        d = $(this).attr("data-user");
+        const token = $(this).attr("data-csrf");
 
         $.ajax({
-            url: '/clicker_app/darkmode/',
-            type: 'POST',
+            url: "/clicker_app/darkmode/",
+            type: "POST",
             success: function (data) {
-                $('#darkmode-button').html(data);
+                $("#darkmode-button").html(data);
             },
-            headers: { 'X-CSRFToken': token },
-            data: { 'user-id': d }
+            headers: { "X-CSRFToken": token },
+            data: { "user-id": d }
         })
     });
 
-    $('.purchase_button').click(function () {
+    $(".purchase_button").click(function () {
         let a, upgrade;
-        a = $(this).attr('data-user');
-        upgrade = $(this).attr('data-upgrade');
-        const token = $(this).attr('data-csrf');
+        a = $(this).attr("data-user");
+        upgrade = $(this).attr("data-upgrade");
+        const token = $(this).attr("data-csrf");
 
         let s1 = "#upgrade-" + String(upgrade) + "-1";
         let s2 = "#upgrade-" + String(upgrade) + "-2";
 
         $.ajax({
-            url: '/clicker_app/purchase/',
-            type: 'POST',
+            url: "/clicker_app/purchase/",
+            type: "POST",
             success: function (data) {
                 $(s1).html(data.cost_instance);
                 $(s2).html(data.quantity);
                 location.reload();
             },
-            headers: { 'X-CSRFToken': token },
-            data: { 'user-id': a, 'upgrade': upgrade }
+            headers: { "X-CSRFToken": token },
+            data: { "user-id": a, "upgrade": upgrade }
         })
     });
 
