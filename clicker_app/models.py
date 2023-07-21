31800081d92b3cb4c54b96c5057f8b70ecc493f4
Thomas McCausland <thomas.mccausland11@gmail.com>
2023-07-21 21:45:31 +0100

    Fix default clicker image on account creation

--- clicker_app/models.py
+++ clicker_app/models.py
@@ -31,7 +31,7 @@ class Account(models.Model):
     achievements = models.ManyToManyField(Achievement, blank=True)
     upgrades = models.ManyToManyField(Upgrade, through="OwnsUpgrade")
     darkmode = BooleanField(default=False)
-    image = models.ImageField(upload_to="uploads/", default=settings.MEDIA_DIR + "/uploads/logo.jpg")
+    image = models.ImageField(upload_to="uploads/", default=settings.MEDIA_DIR + "/uploads/logo.png")
 
     def __str__(self):
         return self.user.username
