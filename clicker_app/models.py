90d09ef5382888569a6d9ac0db70f174a4b39cae
Thomas McCausland <thomas.mccausland11@gmail.com>
2023-08-14 19:35:32 +0100

    Lint

--- clicker_app/models.py
+++ clicker_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import BooleanField, CharField, IntegerField
-from django.conf import settings
 
 
 class Achievement(models.Model):
