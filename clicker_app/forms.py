b3fdb8cddb8825f39f070d829c99fdb553e376f4
Thomas McCausland <thomas.mccausland11@gmail.com>
2023-08-19 17:08:25 +0100

    Update sign up page and form
    Updated signup.html to follow a more logical structure and updated its
    styling to be in line with that of the login template. Also updated the
    meta class for the UserForm for sign up to modify placeholder text,
    labels and help text without having to rework the models.
    
    Closes: #59

--- clicker_app/forms.py
+++ clicker_app/forms.py
@@ -3,11 +3,23 @@ from clicker_app.models import User, Account
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+        labels = {
+            'username': '',
+            'email': '',
+            'password': '',
+        }
+        help_texts = {
+            'username': '',
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email: john@doe.com'}),
+            'password': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+        }
 
 
 class ImageUpload(forms.ModelForm):
